ee5a91e6c2ee73beb995f58f057fb566a09c032c
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-02-24 14:53:07 +0530

    added:singlet logic

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,11 +1,27 @@
 #program for flipcoin combination for github
-
+head_count=0
+tail_count=0
+count=0
+for((i=0;i<20;i++))
+do
 coin=$((RANDOM%2))
 		if(($coin==0))
 		then
-			echo "it is head"
+			((head_count++))
 		elif(($coin==1))
 		then
-			echo "it is tail"
+			((tail_count++))
 		fi
-
+((count++))
+done
+echo "head count is:" $head_count
+echo "tail count is:" $tail_count
+	if(($head_count>$tail_count))
+	then
+	percentage=$(($head_count/$count))
+		echo "head won by" $(($percentage*100))|bc "percentage"
+	elif(($head_count<$tail_count))
+	then
+	percentage=$(($tail_count/$count))
+		echo "tail won by" $(($percentage*100))|bc "percentage"
+	fi
